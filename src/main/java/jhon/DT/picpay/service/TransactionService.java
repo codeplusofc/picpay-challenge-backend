88458feabbf9b7224c6a9318e96c48350a62a825
Jhonatan Santos <jhonatansantos085@gmail.com>
2025-01-09 20:43:33 -0300

    feat: adicionar validações, fluxo de transações e configuração do RestTemplate

--- src/main/java/jhon/DT/picpay/service/TransactionService.java
+++ src/main/java/jhon/DT/picpay/service/TransactionService.java
@@ -1,18 +1,74 @@
 package jhon.DT.picpay.service;
 
+import jhon.DT.picpay.dtos.TransactionDTO;
+import jhon.DT.picpay.model.transaction.Transaction;
+import jhon.DT.picpay.model.user.User;
 import jhon.DT.picpay.repository.TransactionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 public class TransactionService {
 
-    private UserService userService;
+    private final UserService userService;
 
     private final TransactionRepository transactionRepository;
 
-    public TransactionService(TransactionRepository transactionRepository, UserService userService) {
+    private final RestTemplate restTemplate;
+
+    public TransactionService(TransactionRepository transactionRepository, UserService userService, RestTemplate restTemplate) {
         this.transactionRepository = transactionRepository;
         this.userService = userService;
+        this. restTemplate = restTemplate;
+    }
+
+    public void createTransaction(TransactionDTO transactionDTO) throws Exception {
+
+        User sender = this.userService.findUserById(transactionDTO.senderId());
+        User receiver = this.userService.findUserById(transactionDTO.receiverId());
+
+        userService.validateTransaction(sender, transactionDTO.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transactionDTO.value());
+        if (!isAuthorized){
+            throw new Exception("the transaction was not authorized");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transactionDTO.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transactionDTO.value()));
+        receiver.setBalance(receiver.getBalance().add(transactionDTO.value()));
+
+        this.transactionRepository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value){
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+
+        if(authorizationResponse.getStatusCode() == HttpStatus.OK){
+
+            if (authorizationResponse.getBody() != null) {
+                String status = (String) authorizationResponse.getBody().get("status");
+                return "success".equalsIgnoreCase(status);
+            } else {
+                return false;
+            }
+
+        }else{
+            return false;
+        }
     }
 
 
--- src/main/java/jhon/DT/picpay/service/UserService.java
+++ src/main/java/jhon/DT/picpay/service/UserService.java
@@ -1,8 +1,12 @@
 package jhon.DT.picpay.service;
 
 import jhon.DT.picpay.model.user.User;
+import jhon.DT.picpay.model.user.UserType;
 import jhon.DT.picpay.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -18,8 +22,19 @@ public class UserService {
         return this.userRepository.save(user);
     }
 
-    public User findUserById(User user){
-        return this.userRepository.findUserById(user.getId()).orElseThrow(() -> new RuntimeException("User not f"));
+    public User findUserById(UUID id) throws Exception{
+        return this.userRepository.findUserById(id).orElseThrow(() -> new RuntimeException("User not f"));
+    }
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception{
+
+        if (sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("This type of user cannot perform transfers");
+        }
+        if (sender.getBalance().compareTo(amount) <0){
+            throw new Exception("Insufficient balance to perform this transaction");
+
+        }
     }
 
 }
